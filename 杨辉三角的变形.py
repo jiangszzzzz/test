d0e6d0ada6abf8464966d5945d2b0f50534fe5ec
jiangszzzzz <32918248+jiangszzzzz@users.noreply.github.com>
2021-01-30 18:50:50 +0800

    Add files via upload

---
+++ 杨辉三角的变形.py
@@ -0,0 +1,14 @@
+while True:
+    try:
+        a, b = int(input()), 0
+        if a == 1 or a == 2:
+            b = -1
+        elif a%2 == 1 and a > 2:
+            b = 2
+        elif a%4 == 0:
+            b = 3
+        else:
+            b = 4
+        print(b)
+    except:
+        break
